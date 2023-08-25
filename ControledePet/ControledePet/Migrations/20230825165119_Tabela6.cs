7db8ed09a5f16d533c95a0b1feb4551362560da1
Igor Cavalcante <igorcavalcante69@hotmail.com>
2023-08-25 16:11:03 -0300

    Crud

---
+++ ControledePet/ControledePet/Migrations/20230825165119_Tabela6.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControledePet.Migrations
+{
+    public partial class Tabela6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
